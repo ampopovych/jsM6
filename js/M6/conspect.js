f418073cf10a65c4d3382ad6efb76d5f93bc4b0e
Andriy Popovych <viktpopo@gmail.com>
2023-07-09 07:33:54 +0300

    09070733

---
+++ js/M6/conspect.js
@@ -0,0 +1,63 @@
+"use strict";
+// let a, b, c, d; // объявляем все 3 переменные
+
+// // Присваиваем переменным значения:
+// a = 4;
+// b = 2;
+// c = 3;
+// d = c ** a;
+
+// console.log(`Залишок від ділення ${a} на ${b} буде ${d}`);
+
+// let str = `a
+// b
+// c
+// d
+// e
+// f`;
+// console.log(str);
+// let u;
+// console.log(u);
+// let p = null;
+// console.log(p);
+// console.log('abc' * 3);
+// console.log(1 / 0);
+// console.log(-1 / 0);
+// console.log(1 / -0);
+// console.log(123);
+// console.log('123');
+
+// const PI = 3.141645165;
+// console.log(2*PI * 30);
+// let a = +'2';
+// let b = +'3';
+// console.log(a + b);
+// console.log(Number('2s'));
+// let num = parseInt('12px');
+// console.log(num);
+// let num1 = parseFloat('15.4dr');
+// console.log(num1);
+// let a = '5.2px';
+// let b = '4.5px';
+// console.log(`Сума ${a} i ${b} буде ${parseFloat(a) + parseFloat(b)}px`);
+// let c = 5, d = 8;
+// console.log(String(c) + String(d));
+
+// let num = 12345;
+// let num1 = 6789;
+// console.log(`Число ${num} містить ${String(num).length} символів,
+// a число ${num1} містить ${String(num1).length} символів, але сума буде ${num + num1},
+// або якщо додавати рядки, то буде ${String(num) + String(num1)} і символів буде ${(String(num) + String(num1)).length}`);
+// console.log(true - true);
+// console.log(true + false);
+// console.log('1' + true);
+// console.log(String(true) + 1);
+// console.log(String(true) + Number(true));
+// console.log(Boolean(3));
+// console.log(Boolean('true'));
+// console.log(Boolean(1/0));
+let text = 'abcdefghijklmnop';
+for (let index = 0; index < text.length; index +=1) {
+  console.log(text[index]);
+  
+}
