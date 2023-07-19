9186dabc3af87d5098d6ef0016e954b9af9f1c04
Andriy Popovych <viktpopo@gmail.com>
2023-07-19 09:43:51 +0300

    19070943

--- js/M6/repeta.js
+++ js/M6/repeta.js
@@ -1,4 +1,7 @@
-const titlePro 
+const titlePro = document.querySelector('.site_nav');
+console.log(titlePro);
+const navLink = titlePro.querySelectorAll('.site_nav_link');
+console.log(navLink);
 
 
 
